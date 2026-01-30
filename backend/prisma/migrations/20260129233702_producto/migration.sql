65ded59d25ca00c1d8e502d4c1d1f96d6623f434
franqong <garciafranconicolas@hotmail.com>
2026-01-29 21:06:11 -0300

    refactor: prisma structure

---
+++ backend/prisma/migrations/20260129233702_producto/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Producto" (
+    "id" SERIAL NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "descripcion" TEXT,
+    "precio" DECIMAL(10,2) NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "stockMinimo" INTEGER NOT NULL DEFAULT 0,
+    "activo" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Producto_pkey" PRIMARY KEY ("id")
+);
